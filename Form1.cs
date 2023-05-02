a0975f88085a225ea802f36b3a4564f8a3ff0a95
Andrew Pipo <apipo@atechtraining.com>
2023-05-02 16:16:50 -0400

    Added "dotnet run" and "dotnet clean" commands

--- Form1.cs
+++ Form1.cs
@@ -19,6 +19,20 @@ public partial class Form1 : Form
         btnDNETBuild.Width = 200;
         btnDNETBuild.Click += btnDNETBuild_Click;
         this.Controls.Add(btnDNETBuild);
+
+        Button btnDNETRun = new Button();
+        btnDNETRun.Text = "Execute .NET RUN COMMAND";
+        btnDNETRun.Location = new Point(0, 50);
+        btnDNETRun.Width = 200;
+        btnDNETRun.Click += btnDNETBuild_Click;
+        this.Controls.Add(btnDNETRun);
+
+        Button btnDNETClean = new Button();
+        btnDNETClean.Text = "Execute .NET CLEAN COMMAND";
+        btnDNETClean.Location = new Point (0, 75);
+        btnDNETClean.Width = 200;
+        btnDNETClean.Click += btnDNETClean_Click;
+        this.Controls.Add(btnDNETClean);
     }
 
     private void btnDNETBuild_Click(object? sender, EventArgs e)
@@ -36,6 +50,72 @@ public partial class Form1 : Form
                 {
                     FileName = "dotnet",
                     Arguments = "build",
+                    WorkingDirectory = workingDirectory,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (var process = new Process())
+                {
+                    process.StartInfo = processStartInfo;
+                    process.Start();
+
+                    var output = process.StandardOutput.ReadToEnd();
+                    MessageBox.Show(output);
+                }
+            }
+        }
+    }
+
+    private void btnDNETRun_Click(object? sender, EventArgs e)
+    {
+        using (var folderBrowserDialog = new FolderBrowserDialog())
+        {
+            folderBrowserDialog.Description = "Select the working directory for .NET command execution.";
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                var workingDirectory = folderBrowserDialog.SelectedPath;
+
+                // Run the Git command using the selected working directory
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "dotnet",
+                    Arguments = "run",
+                    WorkingDirectory = workingDirectory,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (var process = new Process())
+                {
+                    process.StartInfo = processStartInfo;
+                    process.Start();
+
+                    var output = process.StandardOutput.ReadToEnd();
+                    MessageBox.Show(output);
+                }
+            }
+        }
+    }
+
+    private void btnDNETClean_Click(object? sender, EventArgs e)
+    {
+        using (var folderBrowserDialog = new FolderBrowserDialog())
+        {
+            folderBrowserDialog.Description = "Select the working directory for .NET command execution.";
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                var workingDirectory = folderBrowserDialog.SelectedPath;
+
+                // Run the Git command using the selected working directory
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "dotnet",
+                    Arguments = "clean",
                     WorkingDirectory = workingDirectory,
                     RedirectStandardOutput = true,
                     UseShellExecute = false,
